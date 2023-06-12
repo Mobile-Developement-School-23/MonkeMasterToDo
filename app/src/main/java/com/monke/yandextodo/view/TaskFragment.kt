11be12c875c40a9e4bfabdf48cc53832352f3066
Igor Streshnev <igor.streshnev2019@gmail.com>
2023-06-12 23:04:52 +0300

    task layout

--- app/src/main/java/com/monke/yandextodo/view/TaskFragment.kt
+++ app/src/main/java/com/monke/yandextodo/view/TaskFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputLayout
 import com.monke.yandextodo.R
 
-
 class TaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_task, container, false)
+        val view = inflater.inflate(R.layout.fragment_task, container, false)
+
+        val importanceMenu = view.findViewById<AutoCompleteTextView>(R.id.importance_menu)
+        val arrayAdapter = ArrayAdapter(context!!, R.layout.item_dropdown,
+            resources.getStringArray(R.array.importance_array))
+        importanceMenu.setAdapter(arrayAdapter)
+
+        return view
     }
 
 
